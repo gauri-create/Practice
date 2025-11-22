e28c092626218613ba043f55e91856fcec5cc31c
gauri-create <gauribelokar2005@gmail.com>
2025-11-22 06:32:18 +0530

    Practice

---
+++ Recursion[PaternWise}/CountAllSubsequenceWithSumK.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+    int CountAllSubsequence(vector<int> &nums, int k){
+        vector<int>ds;
+        int n=nums.size();
+        int ans=Count(0, ds, 0, k, nums, n);
+        return ans;
+    }
+    private:
+    int Count(int ind, vector<int>&ds, int s, int sum, vector<int> arr, int n){
+        if(ind==n){
+        //condition satisfiedd
+        if(s==sum) return 1;
+        else 
+           return 0;
+        }
+        s+=arr[ind];
+        int l= Count(ind+1, ds, s, sum,arr, n);
+        s-=arr[ind];
+        int r= Count(ind+1, ds, s, sum,arr, n);
+        return l+r;
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> arr={4,2,10,5,1,3};
+    int sum=5;
+    int ans=obj.CountAllSubsequence(arr, sum);
+    cout<<ans;
+    return 0;
+}
