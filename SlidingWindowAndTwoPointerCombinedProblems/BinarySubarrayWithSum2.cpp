51e60324dc24ed45117053d94f70acf3cd957728
gauri-create <gauribelokar2005@gmail.com>
2025-12-15 05:22:40 +0530

    Practice

---
+++ SlidingWindowAndTwoPointerCombinedProblems/BinarySubarrayWithSum2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    int numberofsubarraySumAtMostGoal(vector<int> & nums, int goal){
+        if(goal <0) return 0;
+        int l=0, r=0, sum=0, cnt=0;
+        while(r<nums.size()){
+            sum += nums[r];
+            while(sum>goal){
+                sum=sum-nums[l];
+                l=l+1;
+            }
+            cnt=cnt+(r-l+1);
+            r=r+1;
+        }
+        return cnt;
+    }
+    public:
+    int numSubarrayWithSum(vector<int> & nums, int goal){
+       return numberofsubarraySumAtMostGoal(nums, goal)-numberofsubarraySumAtMostGoal(nums, goal-1);
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> nums={1,1,0,1,0,0,1};
+    int goal=3;
+    int ans = obj.numSubarrayWithSum(nums, goal);
+    cout<<ans;
+    return 0;
+}
