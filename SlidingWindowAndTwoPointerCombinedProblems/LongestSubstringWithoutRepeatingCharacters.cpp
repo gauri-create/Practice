72a37ab1a71121a489f66de932273ae2e9dd930a
gauri-create <gauribelokar2005@gmail.com>
2025-12-14 19:41:01 +0530

    Practice

---
+++ SlidingWindowAndTwoPointerCombinedProblems/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Solution{
+    public:
+    int longestNonRepeatingSubstring(string &s){
+        map<
+    }
+};
+
+int main(){
+    Solution obj;
+    string s="cadbzabcd";
+    int ans = obj.longestNonRepeatingSubstring(s);
+    cout<<ans;
+    return 0;
+}
