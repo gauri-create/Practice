a9b1bc46fa487e0b4d2edbc1ab196a2e8896755e
gauri-create <gauribelokar2005@gmail.com>
2025-12-14 19:49:51 +0530

    Practice

--- SlidingWindowAndTwoPointerCombinedProblems/LongestSubstringWithoutRepeatingCharacters.cpp
+++ SlidingWindowAndTwoPointerCombinedProblems/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
-#include<map>
+// #include<unordered_map>
 using namespace std;
 
 class Solution{
     public:
     int longestNonRepeatingSubstring(string &s){
-        map<
+        int n=s.size();
+        int Hashlen=256;
+        int hash[Hashlen];
+
+        for(int i=0; i<Hashlen; i++){
+            hash[i]=-1;
+        }
+
+        int l=0, r=0, maxLen=0;
+        while(r<n){
+            if(hash[s[r]] != -1){
+                l=max(hash[s[r]]+1, l);
+            }
+            int len = r-l+1;
+            maxLen=max(maxLen,len);
+            hash[s[r]]=r;
+            r++;
+        }
+        return maxLen;
     }
 };
 
 int main(){
     Solution obj;
-    string s="cadbzabcd";
+    string s="abcddabac";
     int ans = obj.longestNonRepeatingSubstring(s);
     cout<<ans;
     return 0;
