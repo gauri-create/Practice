4c885dce0b4b0f9df470d1f19c5ee000ad863d6b
gauri-create <gauribelokar2005@gmail.com>
2025-12-15 07:38:14 +0530

    Practice

---
+++ SlidingWindowAndTwoPointerCombinedProblems/LongestSubstrigWithAtMostKDistinnctCharacters.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution{
+    public:
+    int kDistinctChar(string &s, int k){
+        int maxlen=0, l=0, r=0;
+        // vector<int>hash(s.size());
+        unordered_map<char, int> mpp;
+        while(r<s.size()){
+            mpp[s[r]]++;
+            if(mpp.size()>k){
+                mpp[s[l]]--;
+                if(mpp[s[l]]==0)mpp.erase(s[l]);
+                l++;
+            }
+            if(mpp.size() <= k){
+                maxlen=max(maxlen, r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
+
+int main(){
+    Solution obj;
+    string s="aababbcaacc";
+    int k=2;
+    int ans = obj.kDistinctChar(s, k);
+    cout<<ans;
+    return 0;
+}
